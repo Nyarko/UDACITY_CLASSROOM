df14e666cd8b34751ff314d0f66be745e50690c7
Nyarko <nyarko.agyekum@yahoo.com>
2022-08-20 20:56:49 +0000

    Finally removed db.create_all() as migrations file which was just created will take care of that.

--- todoapp/app.py
+++ todoapp/app.py
@@ -27,7 +27,6 @@ class TodoList(db.Model):
     name = db.Column(db.String(), nullable=False)
     todos = db.relationship('Todo', backref='list', lazy=True)
 
-#db.create_all()
 
 @app.route('/todos/create', methods=['POST'])
 def create_todo():
